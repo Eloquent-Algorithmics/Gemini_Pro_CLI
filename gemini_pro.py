d4f311a83e0655a2a62e2e5c3079e3fd799506a2
ExplorerGT92 <explorergt92@yahoo.com>
2023-12-15 09:02:10 -0500

    Fixes

--- gemini_pro.py
+++ gemini_pro.py
@@ -1,3 +1,6 @@
+"""
+This script demonstrates how to use the Gemini Pro API.
+"""
 import os
 import base64
 import asyncio
@@ -8,11 +11,9 @@ from vertexai.preview.generative_models import (
     HarmCategory,
     HarmBlockThreshold,
     Part,
-    ChatSession,
 )
 from rich.console import Console
 from rich.markdown import Markdown
-from rich.panel import Panel
 from rich.prompt import Prompt
 from rich.traceback import install
 
@@ -24,9 +25,6 @@ nlp = spacy.load("en_core_web_sm")
 
 # Create a console object for rich printing
 console = Console()
-
-# Load the spaCy model
-nlp = spacy.load("en_core_web_sm")
 
 
 def extract_filename(text):
@@ -57,6 +55,7 @@ def extract_filename(text):
     # print("Extracted filenames:", filenames)
 
     return filenames
+
 
 model = GenerativeModel("gemini-pro")
 chat = model.start_chat(history=[])
@@ -124,7 +123,10 @@ async def ask_gemini_pro_vision(question, source_folder, specific_file_name):
     # Handle the responses
     for response in responses:
         if response.candidates:
-            console.print(response.candidates[0].content.parts[0].text, style="bold green")
+            console.print(
+                response.candidates[0].content.parts[0].text,
+                style="bold green"
+            )
         else:
             console.print("No response candidates found.")
 
@@ -136,7 +138,10 @@ async def main():
 
     console.print(Markdown("# Welcome to Gemini Pro"), style="bold magenta")
     while True:
-        user_input = Prompt.ask("\nAsk your question (or type 'exit' to quit)", default="exit")
+        user_input = Prompt.ask(
+            "\nAsk your question (or type 'exit' to quit)",
+            default="exit"
+        )
         if user_input.lower() == 'exit':
             console.print("\nExiting the program.", style="bold red")
             break
@@ -145,7 +150,6 @@ async def main():
             # Add two blank lines after user input
             console.print("\n")
             if specific_file_names:
-                # console.print("Calling ask_gemini_pro_vision", style="bold yellow")
                 specific_file_name = specific_file_names[0]
                 await ask_gemini_pro_vision(
                     user_input,
@@ -157,6 +161,7 @@ async def main():
                 await ask_gemini_pro(user_input)
             # Add one blank line after the model's response
             # console.print("\n")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
